b44f488d5117f02cf714dbaccbb902cb638ca0d1
Gustavo <gustavo.oliveira-at-124566650657>
2026-06-11 07:57:22 -0300

    Adicionar arquivos de projeto.

---
+++ ExercicioUm.js
@@ -0,0 +1,28 @@
+/*
+ * =========================================================
+ * PROBLEMA: Hello World!
+ * =========================================================
+ *
+ * O seu primeiro programa em qualquer linguagem de
+ * programação normalmente é o "Hello World!". Neste
+ * primeiro problema tudo o que você precisa fazer é
+ * imprimir esta mensagem na tela.
+ *
+ * ---------------------------------------------------------
+ * ENTRADA
+ * ---------------------------------------------------------
+ * Este problema não possui nenhuma entrada.
+ *
+ * ---------------------------------------------------------
+ * SAÍDA
+ * ---------------------------------------------------------
+ * Você deve imprimir a mensagem "Hello World!" e em
+ * seguida o final de linha.
+ *
+ * ---------------------------------------------------------
+ * EXEMPLO
+ * ---------------------------------------------------------
+ * Entrada : (nenhuma)
+ * Saída   : Hello World!
+ * ---------------------------------------------------------
+ */
